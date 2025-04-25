049505c421e2d214143e30434e4187d53790750a
Prashant-Baperkar <prashant.baperkar@gmail.com>
2025-04-25 16:42:35 +0530

    sp

--- test.sql
+++ test.sql
@@ -1,9 +1,10 @@
-use RBL_MISDB_PROD
 
-select * from DimBranch
 
-select * from DimSourceDB_Mod
+create PROCEDURE DIMSOURCEDB_test
+as
+BEGIN
+
 select * from DIMSOURCEDB
 where SourceAlt_Key=1
 
-
+end
